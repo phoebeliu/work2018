11702d6b074f6780cf6656f5c8ba17fddf06a01a
phoebeliu <liujingwen_1110@sina.com>
2019-03-05 16:01:58 +0800

    Update error-bar.js
    add in the popup modal

--- projects/learn-from-work/error-bar.js
+++ projects/learn-from-work/error-bar.js
@@ -105,5 +105,110 @@ link: function (scope, element, attrs, ctrls) {
         //element.removeClass('control-group-focused');
     });
 }
+////in the popup modal
+
+define([],
+    function () {
+        'use strict';
+
+        return [function () {
+            return {
+                restrict: 'A',
+                template: templateStr,
+                scope: {
+                    //blablabla
+                },
+                controller: ['$scope','$state', '$filter', 'WizardService', 'DirtyFlagFactory', 'NavBarUtil', '$window', '$rootScope', function ($scope,$state,$filter,WizardService,DirtyFlagFactory,NavBarUtil,$window,$rootScope) {
+
+                    $scope.validations = new Object();
+                    $scope.screenObj={};
+                    $scope.screenObj.toggleValidationResults= true;
+                   
+
+
+                    /**
+                     * Current there is front-end field issue for the error link of current screen
+                     * @param validation
+                     */
+                    $scope.jumpToIssue = function(validation){
+                        var currentStepName = $state.current.name;
+                        var currentNewLineValidationDom = document.querySelector('#popupValidationDiv .new-line-validation');
+                        if(validation){
+                            if(validation.level == "field"){
+                                var fieldElement = $('#' + validation.fieldId);
+                                if(fieldElement){
+                                    var fieldPosition = fieldElement.offset()?fieldElement.offset().top : 0;
+                                    //var position = $('#'+targetId).offset().top;
+                                    var position2 = $('#'+$scope.popupId+' .modal-header').offset().top;
+                                    fieldPosition = fieldPosition-position2;
+                                    if(currentNewLineValidationDom && !currentNewLineValidationDom.classList.contains('fixed')){
+                                        fieldPosition = fieldPosition - currentNewLineValidationDom.offsetHeight - 44 - 20;
+                                        console.log(currentNewLineValidationDom.offsetHeight);
+                                    }else {
+                                        fieldPosition = fieldPosition  - 44 - 5;//error fix bar height 44px
+                                    }
+                                    if(!NavBarUtil.isLocalHost()){
+                                        console.log("The host is not local!");
+                                        fieldPosition = fieldPosition - 60 - 5;//header height 60px
+                                    }
+                                    fieldElement.find('textarea,input[type]:not([type=search]):not([type=url]):not([type=hidden])').focus();
+                                    //angular.element('html, body').animate({ scrollTop: fieldPosition }, 'slow');
+                                    console.log(validation.fieldId+'         '+fieldPosition);
+                                    $('#' + $scope.popupId).animate({ scrollTop: fieldPosition }, 'slow');
+                                    $scope.screenObj.toggleValidationResults=false;
+                                }
+                            }
+                        }
+
+                    };
+                    /**
+                     * UI fixed
+                     */
+                    $scope.screenObj.showingErrorIndex = 0;
+                    var lineValidationDiv,modalWindow;
+                    $scope.checkOffset =function () {
+                        lineValidationDiv = document.querySelector('#popupValidationDiv .new-line-validation');
+                        modalWindow = document.querySelector('#' + $scope.popupId);
+
+                        if (modalWindow.scrollTop > document.querySelector('#popupValidationDiv').offsetTop) {
+                            lineValidationDiv.classList.add("fixed");
+                            lineValidationDiv.style.top = 0;
+                            $scope.screenObj.toggleValidationResults=false;
+                            $scope.$apply();
+                        } else {
+                            lineValidationDiv.classList.remove("fixed");
+                            $scope.screenObj.toggleValidationResults=true;
+                            $scope.$apply();
+                        }
+                    };
+                    document.querySelector('#' + $scope.popupId).onscroll = function(){
+                        if(document.querySelector('#popupValidationDiv .new-line-validation')){
+                            $scope.checkOffset();
+                        }
+                    };
+                    var destroyHandler = $rootScope.$on("focus-field", function(event, field) {
+                        $scope.isErrorField(field[0].id);
+                    });
+
+                    $scope.isErrorField = function(field){
+                        _.each($scope.validations,function(validation,index){
+                            if(validation && validation.fieldId === field){
+                                $scope.screenObj.showingErrorIndex = index;
+                                $scope.screenObj.toggleValidationResults =false;
+                            }
+                        });
+                    };
+
+                    $scope.$on('$destroy', destroyHandler);
+
+
+                   
+                }]
+            }
+        }]
+
+    })
+
+
 
 
