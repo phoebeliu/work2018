90bdebeaf1d76536489f1077e4cd9b561b6961ce
phoebe.liu <phoebe.j.liu@pwc.com>
2019-09-09 11:38:36 +0800

    http://huziketang.mangojuice.top/books/react/lesson1

--- projects/react-practise/src/Comment.js
+++ projects/react-practise/src/Comment.js
@@ -7,6 +7,44 @@ let users = [
   { username: 'Lucy', age: 20, gender: 'female' , comment:'maybe there are a lot of children.'}
 ]
 
+class AddComment extends Component {
+    submitForm () {
+        console.log('userName');
+        this.setState({
+            usersList: this.props.users
+        })
+    
+        // this.props.likedText = '取消'
+    
+    
+        if (this.props.onClick) {
+          this.props.onClick()
+        }
+    }
+    render () {
+      let { users } = this.props.users
+
+      return (
+        <form className="clearfix">
+        <div className="form-group row">
+            <label htmlFor="userName" className="col-sm-4 col-form-label">User Name : </label>
+            <div className="col-sm-8">
+                <input type="text" className="form-control" id="userName"></input>
+            </div>
+        </div>
+        <div className="form-group row">
+            <label htmlFor="userComment" className="col-sm-4 col-form-label">Comment : </label>
+            <div className="col-sm-8">
+                <textarea className="form-control" id="userComment" rows="3"></textarea>
+            </div>
+        </div>
+        
+        <button type="submit" className="btn btn-primary btn-lg float-right" onClick={this.submitForm.bind(this)}>Submit</button>
+        </form>
+      )
+    }
+  }
+
 class CommentList extends Component {
   render () {
     let { user } = this.props
@@ -24,7 +62,7 @@ class CommentList extends Component {
 class Comment extends Component {
     constructor () {
         super()
-        this.state = { usersList: { users } }
+        this.state = { usersList:  users  }
     }
   render () {
     return (
@@ -33,19 +71,30 @@ class Comment extends Component {
         <span className="navbar-brand mb-0 h1">Navbar</span>
     </nav>
     <div className="container">
-        <h1 className="text-center mt-3">Comment List</h1>
-        <p className="lead">
-        Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.
-        </p>
-        <div className="row justify-content-md-center bg-light pt-3">
-            <div className="col-sm">
-            <ul className="list-group">
-                {/* {users.map((user) => <User user={user} />)} */}
-                {users.map((user, i) => <CommentList key={i} user={this.state.usersList} />)}
-            </ul>
+        <section>
+            <h2 className="text-center mt-3">Add Your Comment Below</h2>
+            <p className="lead">
+            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.
+            </p>
+            <div className="shadow-sm p-3 mb-5 bg-white rounded">
+            <AddComment users={this.state.usersList}></AddComment>
             </div>
-        </div>
-      
+        </section>
+        <hr/>
+        <section>
+            <h1 className="text-center mt-3">Comment List</h1>
+            <p className="lead">
+            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.
+            </p>
+            <div className="row justify-content-md-center bg-light pt-3">
+                <div className="col-sm">
+                <ul className="list-group">
+                    {/* {users.map((user) => <User user={user} />)} */}
+                    {this.state.usersList.map((user, i) => <CommentList key={i} user={user} />)}
+                </ul>
+                </div>
+            </div>
+        </section>
     </div>
     </>
     )
--- why react need manually add key but angular generate hash key  auto.md
+++ why react need manually add key but angular generate hash key  auto.md
@@ -1,3 +1,6 @@
 # why react need manually add key but angular generate hash key  auto
 
 i don't find any answer yet.
+
+http://huziketang.mangojuice.top/books/react/lesson1
+
