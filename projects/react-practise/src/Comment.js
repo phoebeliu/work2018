4fd62c054d37eab5b5d5680e051c4119a274021f
phoebe.liu <phoebe.j.liu@pwc.com>
2019-09-11 16:47:33 +0800

    why clear state and props but input still have value to display???

--- projects/react-practise/src/Comment.js
+++ projects/react-practise/src/Comment.js
@@ -7,11 +7,124 @@ let users = [
   { username: 'Lucy', age: 20, gender: 'female' , comment:'maybe there are a lot of children.'}
 ]
 let missingFileds = '';
+const initInputArrayData = [//This because arrays are reference types in JavaScript
+    {
+        'id': 'userName',
+        'value': '',
+        'label': 'User Name',
+        'type': 'text'
+    },{
+        'id': 'userComment',
+        'value': '',
+        'label': 'Comment',
+        'type': 'textarea'
+    }
+];
+let initInputArray = [
+    {
+        'id': 'userName',
+        'value': '',
+        'label': 'User Name',
+        'type': 'text'
+    },{
+        'id': 'userComment',
+        'value': '',
+        'label': 'Comment',
+        'type': 'textarea'
+    }
+];
+class DataInput extends Component{
+    constructor (props) {
+        super(props)
+        //let  inputObj  = this.props.inputObj;//props undefined need add in constructor (props)
+        this.state = { inputObj : props.inputObj }
+    }
+    componentDidMount() {
+        this.props.onRef(this);
+    }
+    componentWillUnmount() {
+        this.props.onRef(undefined);
+    }
+    clearUp(){
+        let input = this.state.inputObj;
+        //input.value = '';
+        input.value = 'what happened';
+        this.setState({ inputObj : input });
+        this.props.updateFunc(this.state.inputObj,true);
+    }
+    handleChange(event) {
+        let input = this.state.inputObj;
+        input.value = event.target.value;
+        this.setState({ inputObj : input });
+        this.props.updateFunc(this.state.inputObj,true);
+    }
+    renderSwitchInputType(param) {
+        switch(param.type) {
+          case 'text':
+            return <input type="text" className="form-control" id={param.id} value={this.state.inputObj[param.value]} onChange={this.handleChange.bind(this)}></input>;//{this.state[param.value]} will trigger error 'changing an uncontrolled input'
+            //added {this.state[param.value] || ''}
+            //than has init state so remove || ''
+            //{this.state.inputObj[param.value]}
+          case 'textarea':
+            return <textarea className="form-control" id={param.id} value={this.state.inputObj[param.value]} onChange={this.handleChange.bind(this)} rows="3"></textarea>;
+
+          default:
+            return <input type="text" className="form-control"></input>;
+        }
+    }
+    clearThis(e) {
+        e.preventDefault();
+        let input = this.state.inputObj;
+        input.value = '';
+        this.setState({ inputObj : input });
+        this.props.updateFunc(this.state.inputObj,true);
+    }
+    //inputObj={
+    //'id': 'userName',
+    //'value': 'name',
+    //'label': 'User Name',
+    //'type': 'text'
+    //}
+    render(){
+        let  inputObjData  = this.state.inputObj
+        return(
+            <div className="form-group row">
+            <label htmlFor={inputObjData.id} className="col-sm-4 col-form-label">{inputObjData.label} : </label>
+            <div className="col-sm-8">
+                {this.renderSwitchInputType(inputObjData)}
+            </div>
+            <button onClick={this.clearThis.bind(this)} >clear</button>
+        </div>
+        )
+    }
+}
 class AddComment extends Component {
     constructor () {
         super()
-        this.state = { showResults: false }
-      }
+        this.state = { 
+            showResults: false,
+            inputObj : initInputArray,
+            commentData : '',
+            userNameData : ''   
+        }
+    }
+    updateCommentValue (newCommentData,method){
+        if(method){//add
+            let dummyObj = this.state.inputObj;
+            if(newCommentData.id === initInputArray[0].id){
+                dummyObj[0].value = newCommentData.value;
+            }else if(newCommentData.id === initInputArray[1].id){
+                dummyObj[1].value = newCommentData.value;
+            }
+            this.setState({ inputObj:dummyObj});
+        }else{//clear
+            console.log('clear');
+            this.setState({ inputObj: initInputArrayData,commentData : '1111' });
+            for (let i = 0; i < this.state.inputObj.length; i++) {
+                this[`inputComponent${i}`].clearUp();
+            } 
+        }
+    }
     showError(show) {
         this.setState({ showResults: show });
     }
@@ -33,7 +146,8 @@ class AddComment extends Component {
         }else{
             this.showError(false);
             this.props.handleAdd(addUserComment);
-            document.comment.reset();
+            this.updateCommentValue({},false);
+            //document.comment.reset();
         }
         // this.setState({
         //     usersList: this.props.users
@@ -56,7 +170,8 @@ class AddComment extends Component {
         <div role="alert" className={'alert alert-danger ' + (this.state.showResults ? '' : 'd-none')}>{missingFileds} : is missing.</div> 
         
         <form className="clearfix" name="comment">
-        <div className="form-group row">
+        {this.state.inputObj.map((input, i) => <DataInput key={i} inputObj={input} updateFunc={this.updateCommentValue.bind(this)} onRef={ref => (this[`inputComponent${i}`] = ref)}/>)}
+        {/* <div className="form-group row">
             <label htmlFor="userName" className="col-sm-4 col-form-label">User Name : </label>
             <div className="col-sm-8">
                 <input type="text" className="form-control" id="userName"></input>
@@ -67,25 +182,37 @@ class AddComment extends Component {
             <div className="col-sm-8">
                 <textarea className="form-control" id="userComment" rows="3"></textarea>
             </div>
-        </div>
+        </div> */}
         
         <button className="btn btn-primary btn-lg float-right" type="submit" onClick={this.onSubmit.bind(this)}>Submit</button>
         </form>
         </>
       )
     }
-  }
+}
+
+class CommentDetail extends Component{
+    render(){
+        let { user } = this.props
+        return (
+            <li className="list-group-item border-info">
+                <div className="p-3">
+                    <b className="col-3">{user.username} : </b>
+                    <span className="col-9">{user.comment}</span>
+                </div>
+            </li>
+        )
+    }
+}
 
 class CommentList extends Component {
   render () {
-    let { user } = this.props
+    let { users } = this.props
     return (
-        <li className="list-group-item border-info">
-            <div className="p-3">
-                <b className="col-3">{user.username} : </b>
-                <span className="col-9">{user.comment}</span>
-            </div>
-        </li>
+        <ul className="list-group">
+            {/* {users.map((user) => <User user={user} />)} */}
+            {users.map((user, i) => <CommentDetail key={i} user={user} />)}
+        </ul>
     )
   }
 }
@@ -123,10 +250,7 @@ class Comment extends Component {
             </p>
             <div className="row justify-content-md-center bg-light pt-3">
                 <div className="col-sm">
-                <ul className="list-group">
-                    {/* {users.map((user) => <User user={user} />)} */}
-                    {this.state.usersList.map((user, i) => <CommentList key={i} user={user} />)}
-                </ul>
+                    <CommentList users={this.state.usersList} />
                 </div>
             </div>
         </section>
